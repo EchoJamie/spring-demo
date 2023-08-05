a2b1de068125ae6235302e9be7327701124e5faf
EchoJamie <myz0104@163.com>
2023-08-05 23:32:29 +0800

    🎨 项目结构优化及 删除无用代码

--- mybatis/src/main/java/org/jamie/mybatis/proxy/MapperProxy.java
+++ mybatis/src/main/java/org/jamie/mybatis/proxy/MapperProxy.java
@@ -1,18 +1,9 @@
 package org.jamie.mybatis.proxy;
 
-import org.jamie.mybatis.annotation.Param;
-import org.jamie.mybatis.annotation.Select;
-import org.jamie.mybatis.handler.token.ParameterMappingTokenHandler;
-import org.jamie.mybatis.handler.type.TypeHandler;
-import org.jamie.mybatis.handler.type.impl.*;
-import org.jamie.mybatis.parser.GenericTokenParser;
-import org.jamie.mybatis.parser.ParameterMapping;
-
 import javax.sql.DataSource;
 import java.lang.reflect.*;
 import java.sql.*;
 import java.util.*;
-import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -25,19 +16,7 @@ public class MapperProxy implements InvocationHandler {
 
     private final DataSource dataSource;
 
-    private static Map<Class, TypeHandler> typeHandlerMap = new HashMap<>();
-
     private final Map<Method, MapperMethod> methodCache = new ConcurrentHashMap<>();
-
-    static {
-        typeHandlerMap.put(Long.class, new LongTypeHandler());
-        typeHandlerMap.put(Integer.class, new IntegerTypeHandler());
-        typeHandlerMap.put(Short.class, new ShortTypeHandler());
-        typeHandlerMap.put(Float.class, new FloatTypeHandler());
-        typeHandlerMap.put(Double.class, new DoubleTypeHandler());
-        typeHandlerMap.put(String.class, new StringTypeHandler());
-        typeHandlerMap.put(Date.class, new DateTypeHandler());
-    }
 
     public MapperProxy(DataSource dataSource) {
         this.dataSource = dataSource;
